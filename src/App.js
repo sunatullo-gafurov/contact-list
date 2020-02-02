190ed1d1517f905879155702d6fffb9c5d977ab9
Sunatullo Gafurov <me@gmail.com>
2020-02-02 21:21:44 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import ContactList from './Components/ContactList';
 import './App.css';
 
 function App() {
+  const contacts = [
+    {
+      id: 1,
+      name: 'Eddie',
+      online: true,
+      imgUrl: 'https://foreverislamic.files.wordpress.com/2012/09/images1.jpg?w=474',
+    },
+    {
+      id: 2,
+      name: 'Tim',
+      online: false,
+      imgUrl: 'https://www.peacetv.tv/content_files/images/tim_humble_b.jpg',
+    },
+    {
+      id: 3,
+      name: 'Steve',
+      online: true,
+      imgUrl: 'https://i1.wp.com/www.stevedabliz.com/wp-content/uploads/2019/07/31e17915-35ed-420e-bbec-be75ad7bfc59.jpg?fit=1024%2C1024&ssl=1',
+    }
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2 className="title">Contacts</h2>
+      {contacts.map(contact => <ContactList {...contact} />)}
     </div>
   );
 }
---
+++ src/Components/ContactList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function ContactList(props) {
+    const {id, name, online, imgUrl} = props;
+    return (
+        <div key={id} className="contact">
+            <div className="person">
+                <img className="img" src={imgUrl} alt=""/>
+            <div className="status">
+                <div><h3 className="name">{name}</h3></div>
+                <div>{online ? <span className="online">online</span> : <span className="seen">last seen yesterday</span>}</div>
+            </div>
+            </div>
+        </div>
+    );
+}
